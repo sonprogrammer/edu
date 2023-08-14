52ce59b82a4ca72d998113aba10c383595f94030
손영진 <ods04138@gmail.com>
2023-08-14 19:44:09 +0900

    javascript3

--- study/self12.js
+++ study/self12.js
@@ -38,12 +38,61 @@
 
 
 // 공백제거하기
-function removeBlank(a){
-    let result = ""
-    for(let i = 0; i < a.length; i++){
-        if(a[i] !== " "){
-            result += a[i]
-        }
-    }
-    return result
-}
+// function removeBlank(a){
+//     let result = ""
+//     for(let i = 0; i < a.length; i++){
+//         if(a[i] !== " "){
+//             result += a[i]
+//         }
+//     }
+//     return result
+// }
+
+
+// 알파벳 순대로 나열하기
+// function sortStringArray(a){
+//     return a.sort()
+// }
+
+// // 알파벳 반대로 나열하기
+// function sortStringArray(a){
+//     return a.sort().reverse()
+// }
+
+
+// 소수합 구하기
+// function sol(){
+//     let answer = []
+//     let sum =0
+//     for(let i = 2; i <= 200; i++){
+//         let isPrime = true
+//         for(let j = 2; j < i; j++){
+//             if(i % j == 0){
+//                 isPrime = false
+//                 break;
+//             }
+//         }
+//         if(isPrime){
+//             answer.push(i)
+//             sum += i
+//         }
+//     }
+//     return sum
+// }
+
+// document.write(sol())
+
+
+
+
+// 짝수 찾아내기
+// function EvenNumber(arr){
+//     let result = [] 
+//     for(let i = 0; i <= arr.length; i++){
+//     if(arr[i] % 2 === 0){
+
+//         result.push(arr[i])
+//     }
+// }
+//     return result
+// }
