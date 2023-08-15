4d6072e2079c19d6cbb6724b784cd517f603da90
손영진 <ods04138@gmail.com>
2023-08-15 13:47:47 +0900

    class2

---
+++ class/script/class.js
@@ -0,0 +1,17 @@
+console.log('hello js')
+
+var age = 25;
+
+let year = 2023;
+
+const point = 250;
+const today = "광복절"; 
+
+//Number - 숫자
+//String - 문자
+//Boolean - true/false
+//Null, Undefined, Object 
+
+console.log(age)
+console.log(year)
+console.log(point)
