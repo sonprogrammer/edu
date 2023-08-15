cf9ef7337be95c4115c34c43cf73bdca778b3692
손영진 <ods04138@gmail.com>
2023-08-15 14:21:39 +0900

    class3

--- class/script/class.js
+++ class/script/class.js
@@ -7,10 +7,30 @@ let year = 2023;
 const point = 250;
 const today = "광복절"; 
 
+const pass = true;
+
+
+
+console.log(age)
+
+if(pass){
+    console.log('hello') //true면 hello 가찍히고 false면 안찍힘
+}
+
+console.log(typeof(year))
+console.log(typeof(today))
+console.log(typeof(pass))
+
+
+
+
+
 //Number - 숫자
 //String - 문자
 //Boolean - true/false
 //Null, Undefined, Object 
+
+
 
 console.log(age)
 console.log(year)
