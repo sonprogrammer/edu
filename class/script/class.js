28587a88c114daf51040c2251d4be8728cf974aa
손영진 <ods04138@gmail.com>
2023-08-15 15:14:16 +0900

    class3

--- class/script/class.js
+++ class/script/class.js
@@ -4,22 +4,32 @@ var age = 25;
 
 let year = 2023;
 
-const point = 250;
+const point = 2;
 const today = "광복절"; 
 
 const pass = true;
 
+const sale = '7.5';
 
+// console.log(age)
 
-console.log(age)
+// if(pass){
+//     console.log('hello') //true면 hello 가찍히고 false면 안찍힘
+// }
 
-if(pass){
-    console.log('hello') //true면 hello 가찍히고 false면 안찍힘
-}
+// console.log(typeof(year))
+// console.log(typeof(today))
+// console.log(typeof(pass))
 
-console.log(typeof(year))
-console.log(typeof(today))
-console.log(typeof(pass))
+// console.log(year + '_' + today);
+// console.log(point + sale); // 2507.5가 출력됨 ->문자열로 인식되어서
+
+let stringResult = Number(point) * String(year)
+let result = Number(point) * Number(sale)
+
+// console.log(result)
+// console.log(stringResult)
+
 
 
 
@@ -32,6 +42,6 @@ console.log(typeof(pass))
 
 
 
-console.log(age)
-console.log(year)
-console.log(point)
+// console.log(age)
+// console.log(year)
+// console.log(point)
