534469591120929e58ba3aa56e56e7cd4f3fdf4f
손영진 <ods04138@gmail.com>
2023-08-17 10:24:34 +0900

    object1

---
+++ class/script/elice8.js
@@ -0,0 +1,23 @@
+
+// 배달 서비스.
+
+const user = {
+    //객체 속성 + 기능
+    //user 배달을 시키는 고객 정보
+    name : '손영진',
+    age : 27,
+    email : 'ods04138@gmail.com',
+    address : '천안시',
+    food : ['김치찌개', '제육볶음']
+}
+
+
+console.log(user.name);
+console.log(user.age);
+console.log(user);
+console.log(user.food[0]);
+console.log(user.food[1]); //food두번째음식에 접근
+
+
+
+
