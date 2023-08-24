b47a7f866048ea0018ec2df5dedb2cc8450d170b
손영진 <ods04138@gmail.com>
2023-08-24 13:19:15 +0900

    XMLHttpRequest

--- class/script/sandbox.js
+++ class/script/sandbox.js
@@ -1,11 +1,17 @@
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () =>{
-    console.log(request, request.readyState)
+    // console.log(request, request.readyState)
+    if(request.readyState === 4 && request.status === 200){
+        console.log(request.responseText)
+    } else if(request.readyState === 4){ //readyState 가 4라는 건 통신이 완료되엇다는 뜻(파일을 찾든 못찾든)
+        console.log('통신에 장애가 발생했습니다.')
+    }
 })
 
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+// request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+request.open('GET', 'https://jsonplaceholder.typicode.com/todosㅇㄹㄹ/')    //통신장애가 발생했습니다 뜸
 request.send();
 
 // 위에서 GET은 통신 방식을 의미한다  
