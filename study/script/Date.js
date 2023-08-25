da2f7047b6b478dd2f836701b54ef56ba6e7452d
손영진 <ods04138@gmail.com>
2023-08-26 00:43:58 +0900

    more about it

---
+++ study/script/Date.js
@@ -0,0 +1,13 @@
+function now(){
+const currentDate = new Date()
+
+  const year = currentDate.getFullYear()
+  const month = currentDate.getMonth() + 1
+  const day = currentDate.getDate()
+  const hour = currentDate.getHours()
+  const minute = currentDate.getMinutes()
+  const second = currentDate.getSeconds()
+
+return `현재 시간은 ${year}년 ${month}월 ${day}일 ${hour}시 ${minute}분 ${second}초 입니다.`
+}
+console.log(now())
---
+++ study/script/interestRate.js
@@ -0,0 +1,75 @@
+import "./app.css";
+
+const Form = () => {
+  const form = document.getElementById("input-form");
+  const button = document.getElementById("form-button");
+  const result = document.getElementById("result-text");
+
+  button.addEventListener("click", (e) => {
+    const formData = formDataToObject(new FormData(form));
+    // formData 정보를 이용해 복리를 계산하세요.
+    // 적절하게 변환하여, 결과 텍스트를 출력하세요.
+    const {principal, rate, year, frequency} = parseUserInputs(formData)
+    const amount = calculateCompoundInterest(principal, rate, year, frequency)
+    result.innerText = getResultText(principal, rate, year,frequency, amount);
+  });
+
+};
+
+function getResultText(principal, rate, year, frequency, amount){
+    principal = parseMoneyText(principal)
+    frequency = parseFrequencyText(frequency)
+    rate = parseRateText(rate)
+    amount = parseMoneyText(amount)
+    return `예금액 ${principal}원 기준\n${year}년 후 \n ${frequency} ${rate}%의 복리 계산시\n최종 잔고는\n${amount}원 입니다.`
+}
+
+function parseRateText(rate){
+    return `${(rate * 100).toFixed(2)}`
+}
+
+
+function parseMoneyText(principal){
+    return principal.toLocaleString("ko-KR")
+}
+
+function parseFrequencyText(frequency){
+    return{
+        1: "연별",
+        2: "반기별",
+        4: "분기별",
+        12: "월별",
+    }[frequency]
+}
+
+function calculateCompoundInterest(principal, rate, year, frequency){
+    const result =
+        principal * Math.pow(1 + rate / frequency, frequency * year)
+    
+    return Math.floor(result)
+}
+
+
+
+const app = () => {
+  Form();
+};
+
+function parseUserInputs(formData){
+    const{principal, rate, year, frequency} = formData
+    return{
+        principal: Number(principal),
+        rate: Number((rate / 100).toFixed(2)),
+        year: Number(year),
+        frequency: Number(frequency)
+    }
+}
+
+module.exports = app;
+
+function formDataToObject(formData) {
+  return Array.from(formData.entries()).reduce(
+    (acc, [k, v]) => ((acc[k] = Number(v)), acc),
+    {}
+  );
+}
