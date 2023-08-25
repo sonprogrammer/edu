4557ce4eca6999dded9797f56f720d2a5f63ba3e
손영진 <ods04138@gmail.com>
2023-08-25 15:41:43 +0900

    scope2

--- study/script/test.js
+++ study/script/test.js
@@ -10,14 +10,14 @@
 // console.log((callme()))
 
 
-// function varFor(){
-//     var i =0;
-//     for(i = 0; i < 5; ++i){
-//         setTimeout(()=>{
-//             console.log(i)
-//         },1000)
-//     }
-// }
+function varFor(arr){
+    var i =0;
+    for(i = 0; i < arr.length; ++i){
+        setTimeout(()=>{
+            console.log(i)
+        },1000)
+    }
+}
 // function letFor(){
 //     let i =0;
 //     for( i = 0; i < 3; i++){
@@ -27,20 +27,22 @@
 //     }
 // }
 
-// varFor()
+varFor('dfdfdfdsdf')
 // letFor()
 
 
-var f4 = () => console.log('function4')//이러면 정상적으로 출력
-f2()
-// var f4 = () => console.log('function4')//이러면 undefined를 출력
+
+// let f4 = () => console.log('function4')//이래도 정상적으로 출력
+// // var f4 = () => console.log('function4')//이러면 정상적으로 출력
+// f2()
+// // var f4 = () => console.log('function4')//이러면 undefined를 출력
 
 
-// function f4(){
-//     console.log('fucntion4')
+// // function f4(){
+// //     console.log('fucntion4')
+// // }
+
+// function f2(){
+//     console.log('function2')
+//     f4()
 // }
-
-function f2(){
-    console.log('function2')
-    f4()
-}
