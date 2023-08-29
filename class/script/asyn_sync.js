66c21f5e0d3fe22cf8b84b11bf51d5f5c0638824
손영진 <ods04138@gmail.com>
2023-08-29 18:06:31 +0900

    async2

---
+++ class/script/asyn_sync.js
@@ -0,0 +1,24 @@
+// function a() {
+// 	//setTimout3
+// 	//callback3
+// 	setTimeout(() => {
+// 		console.log('1');
+// 	},0);
+// 	console.log('2');
+// }
+
+// //setTimeout1
+// //callback1
+// setTimeout(() => {
+// 	console.log('3');
+// 	a();
+// },0);
+
+// //setTimeout2
+// //callback2
+// setTimeout(() => {
+// 	a();
+// 	console.log('4')
+// },0);
+
+
