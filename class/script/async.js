61678993b68473a7611aafad5118d8a3ccc8a276
손영진 <ods04138@gmail.com>
2023-08-29 11:06:42 +0900

    async

---
+++ class/script/async.js
@@ -0,0 +1,8 @@
+const getData = async () =>{
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const data = await response.json();
+    console.log(data);
+}
+
+getData();
+
