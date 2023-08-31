011d85408d6506ba8f95d03915dc47dcd6fb6b90
손영진 <ods04138@gmail.com>
2023-08-31 14:24:06 +0900

    mini project(weatherapp)2

--- class/script/simple_project2-1.js
+++ class/script/simple_project2-1.js
@@ -1,2 +1,33 @@
 const key = 'kWT1a8O7Kzg2w3ngg2OJoIFstXdGgHbp'
 
+const getWeather = async (id) =>{
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`
+
+    const response = await fetch(base + query)
+    const data = await response.json();
+
+    console.log(data)
+    // return data;
+}
+
+
+//도시 정보 가져오기
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'; //endpoint주소
+    const query = `?apikey=${key}&q=${city}`
+        // http://dataservice.accuweather.com/locations/v1/cities/search?kWT1a8O7Kzg2w3ngg2OJoIFstXdGgHbp=&q=seoul
+
+        const response = await fetch(base + query)
+        const data = await response.json();
+
+        // console.log(data[0])
+        return data[0]
+}
+
+// getCity('newyork')  
+//     .then(data =>{
+//         // console.log(data.key)
+//         return getWeather(data.Key)
+//     })
+//     .catch(err =>console.log(err))
---
+++ class/script/simple_project2.js
@@ -0,0 +1,46 @@
+const cityForm = document.querySelector('form')
+const card = document.querySelector('.card')
+const details = document.querySelector('.details')
+
+const updateUI = (data) =>{
+    const cityName = data.cityInfo;
+    const weather = data.weather;
+
+    console.log(cityName)
+    console.log(weather)
+
+    details.innerHTML = `
+            <h5 class="my-3">${cityName.EnglishName}</h5>
+            <div class="my-3">${weather.WeatherText}</div>
+            <div class="disply-4 my-4">
+              <span>${weather.Temperature.Metric.Value}</span>
+              <span>&deg;C</span>
+            </div>`
+
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none')
+    }
+}
+
+const updateCity = async (city) =>{
+    const cityInfo = await getCity(city)
+    const weather = await getWeather(cityInfo.Key)
+    
+    // console.log(cityInfo)
+    // console.log(weather)
+
+
+    return {cityInfo, weather}
+
+}
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    const city = cityForm.city.value;
+    // console.log(city)       
+    updateCity(city)        
+        .then(data => updateUI(data))
+        // .then(data => updateCity(data))          
+        // .catch(err => console.log(err))
+})
