24bfa13d4cebf1d092781618d650e130a8313a8b
손영진 <ods04138@gmail.com>
2023-09-02 20:34:04 +0900

    caller

---
+++ self_study/script/caller.js
@@ -0,0 +1,29 @@
+// const me = {
+//     name: 'Jimmy'
+//   };
+  
+//   function changeName(person) { //callee
+//     person.name = 'Joo';
+//     console.log(me) //Joo
+//   }
+  
+//   console.log(me); // { name: 'Jimmy' }
+  
+//   changeName(me); //caller
+  
+//   console.log(me); // { name: 'Joo' }
+
+const me = {
+    name: 'Jimmy'
+  };
+  
+  function changeName(person) { //callee
+    person = { name: 'Joo' };
+    console.log(me)
+  }
+  
+  console.log(me); // { name: 'Jimmy' }
+  
+  changeName(me); //caller
+  
+  console.log(me); // 정답은?? jimmy
