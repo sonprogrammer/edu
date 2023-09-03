f05eb4399851026cd8bb075cde2089d633768f60
손영진 <ods04138@gmail.com>
2023-09-03 22:39:19 +0900

    object_method

---
+++ self_study/script/object_method.js
@@ -0,0 +1,25 @@
+// const user = {
+//     name : "Mike",
+//     age : 30,
+//     gender : "male"
+//   }
+
+// console.log(Object.keys(user))
+
+// const user = {
+//     name : "Mike",
+//     age : 30,
+//     gender : "male"
+//   }
+  
+//   console.log(Object.fromEntries(user)); //
+
+
+
+var arr = [10, "문자열", true]; // 배열
+var bool = false; // 불리언
+function func() { return 1; } // 함수
+console.log(arr.toString()); // 10,문자열,true
+console.log(bool.toString()); // false
+console.log(func.toString()); // 함수의 소스 코드가 전부 문자열로 반환됨.
+
