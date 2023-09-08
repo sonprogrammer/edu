f31b599cfce6bd2a7e85b3544b3e24409542b7ec
손영진 <ods04138@gmail.com>
2023-09-08 21:56:55 +0900

    closuer

--- self_study/script/scope.js
+++ self_study/script/scope.js
@@ -40,42 +40,42 @@
 // console.log(hello())
 
 // *!스코프 좀 확실히 이해하자ㅠㅠ
-// let x = '전역 x'
+let x = '전역 x'
 
-// function outer() { //**외부 함수 (outer scope) */
-//     var y = '지역 y'
-//     console.log(x)
-//     console.log(y)
+function outer() { //**외부 함수 (outer scope) */
+    var y = '지역 y'
+    console.log(x)
+    console.log(y)
 
-//     function inner() { //*내부 함수 스코프 (inner scope)
-//         var x = 'inner함수의 지역x'
-//         console.log(x)
-//         console.log(y)
-//     }
-//     inner()
+    function inner() { //*내부 함수 스코프 (inner scope)
+        var x = 'inner함수의 지역x'
+        console.log(x)
+        console.log(y)
+    }
+    inner()
+}
+
+outer()
+console.log(x)
+console.log(y)
+
+
+
+
+// if(true){
+//     let x = 20;
+//     console.log('1', x)
+//     console.log('2',x)
 // }
 
-// outer()
-// console.log(x)
-// console.log(y)
+// console.log('3', x)
 
-
-
-
-if(true){
-    let x = 20;
-    console.log('1', x)
-    console.log('2',x)
-}
-
-console.log('3', x)
-
-function test(){
-    var y = 50;
-    var z = 70
-    console.log('4',x)
-    console.log('5', y)
-}
-test()
-console.log('6', x)
-console.log('7', z)
+// function test(){
+//     var y = 50;
+//     var z = 70
+//     console.log('4',x)
+//     console.log('5', y)
+// }
+// test()
+// console.log('6', x)
+// console.log('7', z)
