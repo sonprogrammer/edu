5187a74305b122639236e9d6c7fe537dd0e9c0d3
손영진 <ods04138@gmail.com>
2023-09-08 21:19:30 +0900

    scope

--- self_study/script/tempCodeRunnerFile.js
+++ self_study/script/tempCodeRunnerFile.js
@@ -1,12 +1,3 @@
-
-// function getApple(){
-//     return new Promise( (resolve, reject) => {
-//       setTimeout(() => resolve("apple"), 1000);
-//     })
-//   }
-  
-//   function getBanana(){
-//     return new Promise( (resolve, reject) => {
-//       setTimeout(() => resolve("banana"), 1000);
-//     })
-//   }
+function third(){
+    //     console.log(x)
+    // }
