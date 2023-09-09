8bac336ed54ed6003ecf7cbc6b9df5292e4addab
손영진 <ods04138@gmail.com>
2023-09-09 17:24:30 +0900

    map

---
+++ self_study/script/map.js
@@ -0,0 +1,14 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7]
+
+function multiplyTwo(number){
+    return number * 2;
+}
+var newNumber = numbers.map(multiplyTwo)
+console.log(newNumber)
+
+var numbers = [4,5, 234,4,5,1]
+
+var result = numbers.map(multiplyTwo)
+console.log(result)
+
+
