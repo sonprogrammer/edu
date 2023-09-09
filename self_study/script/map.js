c787eb0563fe1bd74e70cd52780138b8a8602a8c
손영진 <ods04138@gmail.com>
2023-09-09 17:57:51 +0900

    map2

--- self_study/script/map.js
+++ self_study/script/map.js
@@ -1,14 +1,61 @@
-var numbers = [1, 2, 3, 4, 5, 6, 7]
+// var numbers = [1, 2, 3, 4, 5, 6, 7]
 
-function multiplyTwo(number){
-    return number * 2;
-}
-var newNumber = numbers.map(multiplyTwo)
-console.log(newNumber)
+// function multiplyTwo(number){
+//     return number * 2;
+// }
+// var newNumber = numbers.map(multiplyTwo)
+// console.log(newNumber)
 
-var numbers = [4,5, 234,4,5,1]
+// var numbers = [4,5, 234,4,5,1]
 
-var result = numbers.map(multiplyTwo)
-console.log(result)
+// var result = numbers.map(multiplyTwo)
+// console.log(result)
+
+
+// let students = [
+//     {id: 1, name: 'son'},
+//     {id: 2, name: 'par'},
+//     {id: 3, name: 'in'},
+//     {id: 4, name: 'kim'},
+//     {id: 5, name: 'lee'},
+// ]
+
+// let arr = new Array()
+// for(let obj of students){
+//     arr.push(obj.name)
+// }
+// console.log('1', arr)
+
+// let names = students.map(student => student.name + 1)
+// console.log('2', names)
+
+// const array1 = [1, 4, 9, 16]
+
+// const twice = array1.map(x => x * 2)
+// console.log(twice)
+
+
+
+//*2차원 배열 접근
+// let numbers= [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ]
+
+// let newNumber = numbers.map(array => array.map(x=> x * 2))
+// console.log(newNumber)
+
+
+// let prices={
+//     banana: 1,
+//     apple: 2,
+//     meat: 3
+// }
+
+// let doublePrices = Object.entries(prices).map(([key, value])=> [key, value*2])
+
+
+// console.log(doublePrices)
 
 
