f4ba8d06c9768c618594fa32c889a808f0b10eb0
손영진 <ods04138@gmail.com>
2023-09-12 21:26:21 +0900

    using forEach Method

--- self_study/script/test10.js
+++ self_study/script/test10.js
@@ -18,30 +18,58 @@
 
 
 // 지시사항에 따라 코드를 작성합니다.
-const atm ={
-    currentMoney: 10000,
-    password: 93412,
-    getCurrentMoney: function(){
-        if(password === this.password){
-        return this.currentMoney
-        }else{
-            console.log('check your password')
-        }
+// const atm ={
+//     currentMoney: 10000,
+//     password: 93412,
+//     getCurrentMoney: function(){
+//         if(password === this.password){
+//         return this.currentMoney
+//         }else{
+//             console.log('check your password')
+//         }
         
-   },
-   depositMoney: function(password, put){
-        if(password === this.password){
-            return currentMoney += put
-        }else{
-            console.log('check your password')
-        }
-   },
-   withdrawMoney: function(password, withdraw){
-       if(password === this.password){
-        currentMoney -= withdraw
-       }else{
-           console.log('check your password')
-       }
-   }
-}
-console.log(atm)
+//    },
+//    depositMoney: function(password, put){
+//         if(password === this.password){
+//             return this.currentMoney += put
+//         }else{
+//             console.log('check your password')
+//         }
+//    },
+//    withdrawMoney: function(password, withdraw){
+//        if(password === this.password){
+//         return this.currentMoney -= withdraw
+//        }else{
+//            console.log('check your password')
+//        }
+//    }
+// }
+// console.log(atm)
+
+
+//*forEach 사용 해보기
+// var inputA = [{
+//     name: "박태환",
+//     age: 35
+//   }, 
+//   {
+//     name: "유재석",
+//     age: 49
+//   },
+//   {
+//     name: "김강훈",
+//     age: 12
+//   },
+//   {
+//     name: "이지원",
+//     age: 15
+//   }]
+
+// const makeNameList = (nameList)=>{
+//     let result = ""
+//     nameList.forEach((printName)=>{
+//         result += `<li>${printName.name}</li>`
+//     })
+//     return result
+// }
+// console.log(makeNameList())
