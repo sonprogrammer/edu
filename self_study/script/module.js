1c40e5c08ab6181a8fda28cf9d92b4ce84767b41
손영진 <ods04138@gmail.com>
2023-09-17 21:46:20 +0900

    module

---
+++ self_study/script/module.js
@@ -0,0 +1,9 @@
+function printmilk (t){
+    if(t){
+        for(let i = 0; i< 6; i++){
+            console.log('Buy milk')
+        }
+    }
+}
+
+module.exports = printmilk;
---
+++ self_study/script/module2.js
@@ -0,0 +1,5 @@
+
+const printmilk = require('./module')
+
+// 가져온 모듈의 매개변수에 true를 전달하여 결과를 확인하세요.
+printmilk(true)
