2459cbe6e641b1af16cd426089df0a6bf8b5b030
손영진 <ods04138@gmail.com>
2023-09-27 22:00:21 +0900

    promise

--- class/script/promise2.js
+++ class/script/promise2.js
@@ -243,35 +243,53 @@
 // }
 
 
-import "babel-polyfill";
-//fetch()
-async function fetchImage() {
-  //1. fetch() 메서드를 호출하여 구글에 검색된 햄버거 이미지를 가져오고 변수에 저장하세요. 이때 await을 사용하세요
-  let response = await fetch(
-    "https://media.istockphoto.com/photos/cheeseburger-isolated-on-white-picture-id1157515115?k=20&m=1157515115&s=612x612&w=0&h=1-tuF1ovimw3DuivpApekSjJXN5-vc97-qBY5EBOUts="
-  );
+// import "babel-polyfill";
+// //fetch()
+// async function fetchImage() {
+//   //1. fetch() 메서드를 호출하여 구글에 검색된 햄버거 이미지를 가져오고 변수에 저장하세요. 이때 await을 사용하세요
+//   let response = await fetch(
+//     "https://media.istockphoto.com/photos/cheeseburger-isolated-on-white-picture-id1157515115?k=20&m=1157515115&s=612x612&w=0&h=1-tuF1ovimw3DuivpApekSjJXN5-vc97-qBY5EBOUts="
+//   );
 
-  const error = false;
+//   const error = false;
 
-  if (!response.ok || error) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  } else {
-    document.write("here's the image");
-    return await response.blob();
-  }
+//   if (!response.ok || error) {
+//     throw new Error(`HTTP error! status: ${response.status}`);
+//   } else {
+//     document.write("here's the image");
+//     return await response.blob();
+//   }
+// }
+// // then() 블록을 사용하여 promise가 성공적인 실행에 응답합니다.
+// // 반환된 응답을 받아 blob()을 실행하여 blob으로 변환합니다.
+// fetchImage()
+//   .then((blob) => {
+//     // Blob 객체를 가리키는 객체 URL을 생성
+//     let objectURL = URL.createObjectURL(blob);
+//     // blob(이미지)을 표시할 <img> 요소를 만듭니다.
+//     let image = document.createElement("img");
+//     // <img>의 src를 객체 URL로 설정하여 이미지에 표시되도록 합니다.
+//     image.src = objectURL;
+
+//     // ADOM에 <img> 요소 추가
+//     document.body.appendChild(image);
+//   })
+//   .catch((e) => console.log(e));
+
+
+
+//비동기 3초 타이머 
+
+const clickBtn = () =>{
+  setTimeout(()=>{
+    console.log( '1초<br>')
+  }, 1000)
+  setTimeout(()=>{
+    console.log( '2초<br>')
+  }, 2000)
+  setTimeout(()=>{
+    console.log( '3초<br>')
+  }, 3000)
 }
-// then() 블록을 사용하여 promise가 성공적인 실행에 응답합니다.
-// 반환된 응답을 받아 blob()을 실행하여 blob으로 변환합니다.
-fetchImage()
-  .then((blob) => {
-    // Blob 객체를 가리키는 객체 URL을 생성
-    let objectURL = URL.createObjectURL(blob);
-    // blob(이미지)을 표시할 <img> 요소를 만듭니다.
-    let image = document.createElement("img");
-    // <img>의 src를 객체 URL로 설정하여 이미지에 표시되도록 합니다.
-    image.src = objectURL;
 
-    // ADOM에 <img> 요소 추가
-    document.body.appendChild(image);
-  })
-  .catch((e) => console.log(e));
+console.log(clickBtn())
