473b074c69f9ea65004598f5ef9724e513685345
손영진 <ods04138@gmail.com>
2023-09-30 20:16:52 +0900

    backEnd

--- self_study/practice/my_app/index.js
+++ self_study/practice/my_app/index.js
@@ -3,6 +3,7 @@ import path from 'path';
 import nunjucks from 'nunjucks';
 import bodyParser from 'body-parser';
 import fs from 'fs'
+import mongoose from 'mongoose'
 
 const app = express();
 
@@ -25,11 +26,35 @@ nunjucks.configure('views', {
     express: app
 })
 
+// mongoose connect
+mongoose.connect('mongodb://127.0.0.1:27017')
+    .then(() =>{
+        console.log('db연결 성공')
+    })
+    .catch(e => console.log(e))
+
+//mongoose 세팅
+const { Schema } = mongoose;
+
+const WritingSchema = new Schema({
+    title: String,
+    contents: String,
+    date:{
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Writing = mongoose.model('Writing', WritingSchema)
+
 // middleware
 // main page GET
 app.get('/', async (req, res) => {
-    const fileData = fs.readFileSync(filePath)
-    const writings = JSON.parse(fileData)
+    // const fileData = fs.readFileSync(filePath)
+    // const writings = JSON.parse(fileData)
+
+ 
+    let writings = await Writing.find({})
 
     // console.log(writings)
     res.render('main', { list: writings });
@@ -42,31 +67,89 @@ app.get('/write', (req, res) => {
 app.post('/write', async (req, res) => {
     const title = req.body.title;
     const contents = req.body.contents;
-    const date = req.body.date;
+
 
     //데이터 저장
     //Data/writing.json안에 글 내용이 저장
-    const fileData = fs.readFileSync(filePath); //파일 읽기
-    // console.log(fileData)
+    // const fileData = fs.readFileSync(filePath); //파일 읽기
+    // // console.log(fileData)
 
-    const writings = JSON.parse(fileData) //파일 변환 
-    // console.log(writings)
+    // const writings = JSON.parse(fileData) //파일 변환 
+    // // console.log(writings)
 
-    //request 데이터를 저장
-    writings.push({
-        'title': title,
-        'contents': contents,
-        'date': date
+    // //request 데이터를 저장
+    // writings.push({
+    //     'title': title,
+    //     'contents': contents,
+    //     'date': date
+    // })
+
+    // //data/writing.json에 저장하기
+    // fs.writeFileSync(filePath, JSON.stringify(writings))
+
+    //mongodb에 저장
+    const writing = new Writing({
+        title: title,
+        contents: contents
     })
 
-    //data/writing.json에 저장하기
-    fs.writeFileSync(filePath, JSON.stringify(writings))
+    const result = await writing.save().then(()=>{
+        console.log('success')
+        res.render('detail', { 'detail': { title: title, contents: contents } });
 
-    res.render('detail', { 'detail': { title: title, contents: contents, date: date } });
+    })
+    .catch((err)=>{
+        console.log(err)
+        res.render('write')
+    })
+
 });
 
-app.get('/detail', async (req, res) => {
-    res.render('detail');
+app.get('/detail/:id', async (req, res) => {
+    const id = req.params.id
+
+    const detail = await Writing.findOne({ _id: id})
+        .then((result)=>{
+            res.render('detail', {'detail': detail})
+        })
+        .catch((err)=>{
+            console.error(err)
+        })
+    // res.render('detail');
+})
+
+app.get('/edit/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const edit = await Writing.findOne({ _id: id }).then((result) => {
+        res.render('detail', { 'edit': result })
+    }).catch((err) => {
+        console.error(err)
+    })
+})
+
+app.post('/edit/:id', async (req, res) => {
+    const id = req.params.id;
+    const title = req.body.title;
+    const contents = req.body.contents;
+
+    const edit = await Writing.replaceOne({ _id: id }, { title: title, contents: contents }).then((result) => {
+        console.log('update success')
+        res.render('detail', { 'detail': { 'id': id, 'title': title, 'contents': contents } });
+    }).catch((err) => {
+        console.error(err)
+    })
+})
+
+app.post('/delete/:id', async(req, res)=>{
+    const id = req.params.id;
+    const delete_content = await Writing.deleteOne({ _id: id})
+        .then(()=>{
+            console.log('delete success')
+            res.redirect('/')
+        }).catch((err) =>{
+            console.error(err)
+        })
 })
 
 app.listen(3000, () => {
