5de5387f6b39d2ac2bd64e75841dd337e65d3d4e
youngjin son <ods04138@gmail.com>
2023-10-26 21:59:45 +0900

    stateHook

---
+++ self_study/react/practic/stateHook.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  // name과 email에 대한 State를 각각 선언하세요.
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  return (
+    <>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={({ target: { value } }) => setName(value)}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={({ target: { value } }) => setEmail(value)}
+        />
+      </label>
+      <br />
+      <h3>
+        {" "}
+        이름: {name} 이메일: {email}{" "}
+      </h3>
+    </>
+  );
+}
+
+export default App;
