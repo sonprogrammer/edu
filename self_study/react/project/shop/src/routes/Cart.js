7289a54abc678af09d47256753c1c929c25aad2c
youngjin son <ods04138@gmail.com>
2023-11-07 17:38:55 +0900

    redux3

--- self_study/react/project/shop/src/routes/Cart.js
+++ self_study/react/project/shop/src/routes/Cart.js
@@ -1,15 +1,16 @@
-import {Table} from 'react-bootstrap'
+import { Table } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
 
 function Cart() {
-
-    const a = useSelector((state)=>{return state})
+  const carts = useSelector((state) => {
+    return state.cart
+  })
 
   return (
     <div>
       <Table>
         <thead>
-          <tr> 
+          <tr>
             {/* tr은 행, th,td는 열 */}
             <th>#</th>
             <th>상품명</th>
@@ -19,13 +20,19 @@ function Cart() {
         </thead>
         {/* thead는 제목부분 */}
         <tbody>
-        {/* tbody는 내용부분 */}
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>1</td>
-            <td>수정하기</td>
-          </tr>
+          {/* tbody는 내용부분 */}
+
+          {carts.map((cart, i) => {
+            return (
+              <tr key={i}>
+                <td>{carts[i].id}</td>
+                <td>{carts[i].name}</td>
+                <td>{carts[i].count}</td>
+                <td>수정하기</td>
+              </tr>
+            )
+          })}
+        
         </tbody>
       </Table>
     </div>
--- self_study/react/project/shop/src/store.js
+++ self_study/react/project/shop/src/store.js
@@ -1,12 +1,21 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 const user = createSlice({
-    name: 'user',
-    initialState: 'kim'
+  name: 'user',
+  initialState: 'kim',
+})
+
+const cart = createSlice({
+  name: 'cart',
+  initialState: [
+    { id: 0, name: 'White and Black', count: 2 },
+    { id: 2, name: 'Grey Yordan', count: 1 },
+  ],
 })
 
 export default configureStore({
-  reducer: { 
-    user: user.reducer
-  }
-}) 
+  reducer: {
+    user: user.reducer,
+    cart: cart.reducer,
+  },
+})
