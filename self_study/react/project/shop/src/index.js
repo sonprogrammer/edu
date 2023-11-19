d7dd93ade391b0f7bdde12240c6fbcea5a9a739f
youngjin son <ods04138@gmail.com>
2023-11-19 16:25:47 +0900

    practice

--- self_study/react/project/shop/src/index.js
+++ self_study/react/project/shop/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
+import App1 from './App1'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
@@ -15,7 +15,7 @@ root.render(
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <App1 />
       </BrowserRouter>
     </Provider>
   </QueryClientProvider>
