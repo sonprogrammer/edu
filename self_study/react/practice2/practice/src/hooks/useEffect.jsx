5b0bd53965f1524a8d397ceaeeab9c8b70f6d5fb
youngjin son <ods04138@gmail.com>
2024-01-04 17:45:16 +0900

    Change : weather icon

--- self_study/react/practice2/practice/src/hooks/useEffect.jsx
+++
@@ -1,9 +0,0 @@
-import React,{useEffect, useState} from "react";
-
-function Counters(props) {
-    const [count, setCount] = useState(0)
-
-    useEffect(() =>{
-        document.title = `you clicked ${count} times`;
-    })
-}
