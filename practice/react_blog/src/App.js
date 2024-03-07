c63b9104e6e977fff84404e1b0551642eb3b343c
sonprogrammer <ods04138@gmail.com>
2024-03-07 18:25:31 +0900

    react practice

--- practice/react_blog/src/App.js
+++ practice/react_blog/src/App.js
@@ -12,6 +12,8 @@ function App() {
   ]);
   let [like, setLike] = useState(0)
   let [modal, setModal] = useState(false)
+  let [selectedTitle, setSelectedTitle] = useState(null)
+  let [title, setTitle] = useState(0)
 
   function likebtn(i){
     let copy = [...titles]
@@ -19,27 +21,20 @@ function App() {
     setTitles(copy)
   }
 
-  function sortbtn(){
-    let copy = [...title]
-    copy.sort()
-    setTitle(copy)
-  }
-  
   return (
     <div className="App">
       <div className="black-nav">
         <h4>Blog</h4>
       </div>
-      <button onClick={sortbtn}>sort</button>
         {titles.map((a,i) => (          
-          <div className="list" onClick={()=>setModal(!modal)}>
-            <h4>{a.text} <span onClick={()=>likebtn(i)}>👍🏿</span> {a.likes}</h4>
+          <div className="list">
+            <h4 onClick={()=>{setModal(!modal); setTitle(i)}}>{a.text} <span onClick={()=>likebtn(i)}>👍🏿</span> {a.likes}</h4>
             <p>date</p>
           </div>
         ))}
 
         {
-         modal == true ?  <Modal /> : null
+         modal == true ?  <Modal title={title} titles={titles}/> : null
         }
     </div>
   );
@@ -47,12 +42,14 @@ function App() {
 
 export default App;
 
-function Modal(){
+function Modal(props){
   return(
-    <div className='modal'>
-      <h4>title</h4>
-      <p>date</p>
-      <p>detail</p>
-    </div>
+
+        <div className='modal'>
+          <h4>{props.titles[props.title].text}</h4>
+          <p>date</p>
+          <p>detail</p>
+        </div>
+
   )
 }
