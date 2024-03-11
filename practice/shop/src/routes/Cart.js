e7b044f3458cc7aa71abfd42f724bdafb6fb4640
sonprogrammer <ods04138@gmail.com>
2024-03-11 14:30:17 +0900

    practice react

--- practice/shop/src/routes/Cart.js
+++ practice/shop/src/routes/Cart.js
@@ -1,12 +1,20 @@
 import { Table } from "react-bootstrap"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { changeName, increase, plusCount } from '../store'
 
 export default function Cart() {
 
     let a = useSelector((state)=>{ return state})
     console.log(a.cart)
 
+    let dispatch = useDispatch()
+
   return (
+    <>
+    <h6>{a.user.name} {a.user.age}</h6>
+    <button onClick={()=>{
+        dispatch(increase(100))
+    }}>button</button>
     <Table>
       <thead>
         <tr>
@@ -22,9 +30,15 @@ export default function Cart() {
           <td>{a.cart[i].id}</td>
           <td>{a.cart[i].name}</td>
           <td>{a.cart[i].count}</td>
+          <td>
+            <button onClick={()=>{
+                dispatch(plusCount(i))
+            }}>+</button>
+          </td>
         </tr>
         )}
       </tbody>
     </Table>
+    </>
   )
 }
--- practice/shop/src/store.js
+++ practice/shop/src/store.js
@@ -3,8 +3,21 @@ import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 let user = createSlice({
     name: 'user',
-    initialState: 'son'
+    initialState: {name : 'son', age: 28},
+
+    reducers:{
+        changeName(state){
+            state.name = 'youngjin'
+        },
+        increase(state, action){
+            state.age += action.payload
+        }
+    }
+
 })
+
+export let {changeName, increase}  =  user.actions
+
 let stock = createSlice({
     name: 'stock',
     initialState: [10,11,12]
@@ -15,8 +28,15 @@ let cart = createSlice({
     initialState: [
         {id : 0, name : 'White and Black', count : 2},
         {id : 2, name : 'Grey Yordan', count : 1}
-      ] 
+      ] ,
+    reducers:{
+        plusCount(state,a){
+            state[a.payload].count++;
+        }
+    }
 })
+
+export let { plusCount } = cart.actions
 
 export default configureStore({
   reducer: { 
