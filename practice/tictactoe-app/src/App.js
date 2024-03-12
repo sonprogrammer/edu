c5853d0af6dca6c210766d4f99c0f158796c4c49
sonprogrammer <ods04138@gmail.com>
2024-03-12 19:07:46 +0900

    tictactoe react

--- practice/tictactoe-app/src/App.js
+++ practice/tictactoe-app/src/App.js
@@ -5,6 +5,7 @@ import Board from './Components/Board';
 function App() {
   const [history, setHistory] = useState([{squares: Array(9).fill(null)}])
   const [xIsNxt, setXIsNext] = useState(true)
+  const [stepNumber, setStepNumber] = useState(0)
 
   const calculateWinner =(squares) =>{
     const lines = [
@@ -25,7 +26,7 @@ function App() {
     }
     return null
 }
-const current = history[history.length -1]
+const current = history[stepNumber]
 const winner = calculateWinner(current.squares)
 
 
@@ -38,13 +39,31 @@ let status
   
 
 const handleClick = (i) =>{
-  const newSquares = current.squares.slice();
+  const newHistory = history.slice(0, stepNumber+1)
+  const newCurrent = newHistory[newHistory.length-1]
+  const newSquares = newCurrent.squares.slice();
   if(calculateWinner(newSquares) || newSquares[i]){
     return
   }
   newSquares[i] = xIsNxt ? 'X': 'O'
-  setHistory([...history, {squares: newSquares}])
+  setHistory([...newHistory, {squares: newSquares}])
   setXIsNext(prev => !prev)
+
+  setStepNumber(newHistory.length)
+}
+
+const moves = history.map((step, move)=>{
+  const desc = move ? 'Go to move #' + move : 'Go to game start'
+  return(
+    <li key={move}>
+      <button onClick={()=>jumpTo(move)}>{desc}</button>
+    </li>
+  )
+})
+
+function jumpTo(step){
+  setStepNumber(step)
+  setXIsNext((step % 2) === 0)
 }
 
   return (
@@ -54,6 +73,9 @@ const handleClick = (i) =>{
       </div>
       <div className="game-info">
         <div className='status'>{status}</div>
+        <ol>
+          {moves}
+        </ol>
       </div>
     </div>
   );
