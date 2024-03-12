d5da2a0328fc57012c8c24046a8e64fb841e834b
sonprogrammer <ods04138@gmail.com>
2024-03-12 15:29:11 +0900

    tictactoe react

---
+++ practice/tictactoe-app/src/Components/Square.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Square.css'
+
+export default class Square extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            value: null
+        }
+    }
+    
+    render(){
+        return(
+            <button className='square' onClick={()=>{this.setState({value: 'X'})}}>
+                {this.state.value}
+            </button>
+        )
+    }
+}
+
