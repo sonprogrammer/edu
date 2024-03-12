4c340cde8f7b6ca329fde94e12db573a7dc92f38
sonprogrammer <ods04138@gmail.com>
2024-03-12 18:48:23 +0900

    tictactoe react

--- practice/tictactoe-app/src/App.js
+++ practice/tictactoe-app/src/App.js
@@ -1,14 +1,59 @@
+import { useState } from 'react';
 import './App.css';
 import Board from './Components/Board';
 
 function App() {
+  const [history, setHistory] = useState([{squares: Array(9).fill(null)}])
+  const [xIsNxt, setXIsNext] = useState(true)
+
+  const calculateWinner =(squares) =>{
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ]
+    for(let i = 0; i < lines.length; i++) {
+        const [a,b,c] = lines[i]
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a]
+        }
+    }
+    return null
+}
+const current = history[history.length -1]
+const winner = calculateWinner(current.squares)
+
+
+let status
+    if(winner){
+        status = 'Winner : ' + winner
+    }else{
+        status = `Next player: ${xIsNxt ? 'X' : 'O'}`
+    }
+  
+
+const handleClick = (i) =>{
+  const newSquares = current.squares.slice();
+  if(calculateWinner(newSquares) || newSquares[i]){
+    return
+  }
+  newSquares[i] = xIsNxt ? 'X': 'O'
+  setHistory([...history, {squares: newSquares}])
+  setXIsNext(prev => !prev)
+}
+
   return (
     <div className='game'>
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} onClick={(i)=>handleClick(i)}/>
       </div>
       <div className="game-info">
-
+        <div className='status'>{status}</div>
       </div>
     </div>
   );
--- practice/tictactoe-app/src/Components/Board.js
+++ practice/tictactoe-app/src/Components/Board.js
@@ -2,58 +2,15 @@ import React, { useState } from 'react'
 import Square from './Square'
 import './Board.css'
 
-const Board =() => {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [xIsNxt, setXIsNext] = useState(true)
-
-    const calculateWinner =(squares) =>{
-        const lines = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6],
-        ]
-        for(let i = 0; i < lines.length; i++) {
-            const [a,b,c] = lines[i]
-            if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-                return squares[a]
-            }
-        }
-        return null
-    }
-    
-    const winner = calculateWinner(squares)
-    let status
-    if(winner){
-        status = 'Winner : ' + winner
-    }else{
-        status = `Next player: ${xIsNxt ? 'X' : 'O'}`
-    }
-    
-    
-    const handleClick = (i) =>{
-        const newSquares = squares.slice()
-
-        if(calculateWinner(newSquares) || newSquares[i]){
-            return
-        }
-        
-        newSquares[i] = xIsNxt ? 'X' : 'O'
-        setSquares(newSquares)
-        setXIsNext(current => !current)
-    }
+const Board =({squares, onClick}) => {
     
     const renderSquare = (i) =>{
-        return <Square value={squares[i]} onClick={()=> handleClick(i)}/>
+        return <Square value={squares[i]} onClick={()=> onClick(i)}/>
     }
 
     return (
       <div>
-        <div className='status'>{status}</div>
+
         <div className='board-row'>
             {renderSquare(0)}
             {renderSquare(1)}
