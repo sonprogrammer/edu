68eb9cb47bdeb3f1f58ded1f2817c0e3a29e5371
sonprogrammer <ods04138@gmail.com>
2024-03-12 15:53:33 +0900

    tictactoe react

--- practice/tictactoe-app/src/Components/Board.js
+++ practice/tictactoe-app/src/Components/Board.js
@@ -4,8 +4,22 @@ import './Board.css'
 
 export default class Board extends Component {
 
+
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        }
+    }
+    
+    handleClick(i){
+        const squares = this.state.squares.slice()
+        squares[i] = 'X'
+        this.setState({squares: squares})
+    }
+    
     renderSquare(i){
-        return <Square value={i}/>
+        return <Square value={this.state.squares[i]} onClick={()=> this.handleClick(i)}/>
     }
 
   render() {
--- practice/tictactoe-app/src/Components/Square.js
+++ practice/tictactoe-app/src/Components/Square.js
@@ -2,18 +2,11 @@ import React from 'react';
 import './Square.css'
 
 export default class Square extends React.Component {
-
-    constructor(props){
-        super(props);
-        this.state={
-            value: null
-        }
-    }
     
     render(){
         return(
-            <button className='square' onClick={()=>{this.setState({value: 'X'})}}>
-                {this.state.value}
+            <button className='square' onClick={()=>{this.props.onClick()}}>
+                {this.props.value}
             </button>
         )
     }
